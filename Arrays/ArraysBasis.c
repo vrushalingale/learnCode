501ff9cbb212708f1430e3fcaff7c26cad219995
Vrushal <ingalevrushal@gmail.com>
2023-09-05 21:59:04 +0530

    Arrays Insertion by 3 ways : sorted_AtPosition_end

--- Arrays/ArraysBasis.c
+++ Arrays/ArraysBasis.c
@@ -3,11 +3,13 @@
 void main()
 {
     int myNumbers[] = {25, 50, 75, 100};
+
     printf("Array Before Changing Value :");
-    for(int i=0; i<4;i++){
+    for (int i = 0; i < 4; i++)
+    {
         printf("\n%d", myNumbers[i]);
     }
-    
+
     myNumbers[0] = 33;
 
     printf("\nArray After Changing Value : %d", myNumbers[0]);
