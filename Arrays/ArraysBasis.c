77cd9ae49a1105715c56302b13d790c3edf48415
Vrushal <ingalevrushal@gmail.com>
2023-09-05 11:39:54 +0530

    learning Arrays Basis_01

--- Arrays/ArraysBasis.c
+++ Arrays/ArraysBasis.c
@@ -3,11 +3,14 @@
 void main()
 {
     int myNumbers[] = {25, 50, 75, 100};
+    printf("Array Before Changing Value :");
+    for(int i=0; i<4;i++){
+        printf("\n%d", myNumbers[i]);
+    }
     
-    printf("Before Changing Value : %d", myNumbers[0]);
     myNumbers[0] = 33;
 
-    printf("\nAfter Changing Value : %d", myNumbers[0]);
+    printf("\nArray After Changing Value : %d", myNumbers[0]);
 
     int i;
 
